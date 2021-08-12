cd9647bccf6d43655a2887429bd0427e41de294f
Vanesa Cifuentes <vannehifu2020@gmail.com>
2021-08-11 21:02:16 -0500

    make it better

--- src/app/routes/inventory.js
+++ src/app/routes/inventory.js
@@ -1,6 +1,10 @@
 const { createPool } = require('mysql');
 const app = require('../../config/server')
-const pool = require('../../config/db')
+const pool = require('../../config/db');
+const { connect } = require('../../config/server');
+const e = require('express');
+
+var datos_usuario;
 
 module.exports = app => {
     app.get('/', (req, res) => {
@@ -30,7 +34,7 @@ module.exports = app => {
     })
 
     app.post('/registro', async(req, res) => {
-        const { nombre, apellido, correo, telefono, pass, subject} = req.body;
+        const { nombre, apellido, correo, telefono, pass, grupo, } = req.body;
 
         /*console.log(req.body);*/
 
@@ -40,10 +44,10 @@ module.exports = app => {
             correo: correo,
             telefono: telefono,
             pass: pass,
-            subject: subject
+            grupo: grupo
         };
 
-        pool.query('INSERT INTO usuarios2 set ?', newUser, (err, results) => {
+        pool.query('INSERT INTO usuarios set ?', newUser, (err, results) => {
             if (err) {
               console.log(err);
             } else {
@@ -57,16 +61,92 @@ module.exports = app => {
     app.post('/login', (req,res) => {
         const {email,password} = req.body
         if(email && password){
-            pool.query('SELECT * FROM usuarios2 WHERE correo = ?', [email], async (error,result) => {
+            pool.query('SELECT * FROM usuarios WHERE correo = ?', [email], async (error,result) => {
                 if(result.length === 0 || !(password === result[0].pass)){
                     res.redirect("/")
                 }else{
+                    datos_usuario=result[0];
+                    console.log(datos_usuario);
                     req.session.loggedin=true; 
                     req.session.name=result[0].nombre 
                     res.redirect("/")
                 }
-            });
-
+            })
         }
+    });
+
+    //pides los datos a la base de datos (Del titulo y del contenido del blog)
+    //res.render('adulto mayor.ejs',{titulo: titulo(Traida de la base de datos), contenido})
+
+    app.post('/contactanos', (req,res) => {
+        const {inputName,inputEmail,inputPhone,inputMessage} = req.body
+        pool.query('INSERT INTO contactanos SET ?', {
+            nombre_completo: inputName,
+            correo: inputEmail,
+            telefono: inputPhone,
+            mensaje: inputMessage
+        },(err, result)=> {
+            if(err) {
+                console.log(err);
+            }else{
+                res.redirect('/')
+            }
+        });
+        
+    });
+    app.post('/Unirme', (req,res) => {
+        const {inputName,inputEmail,inputGrupo} = req.body
+        pool.query('INSERT INTO grupo SET ?', {
+            nombre_completo: inputName,
+            correo: inputEmail,
+            grupo: inputGrupo
+        },(err, result)=> {
+            if(err) {
+                console.log(err);
+            }else{
+                res.redirect('/')
+            }
+        });
+    });
+
+
+    app.post("/AdultoMayor", (req, res) =>{
+        let id_usuario= req.session.id;
+        let id_grupo = req.param.id;
+
+        console.log(id_usuario, id_usuario);
+
+        connect.query("INSERT INTO r_usuarios_grupo SET ?",{
+            id_usuario:id_usuario,
+            id_grupo: id_grupo,
+        },
+            (error, result) =>{
+                if(error, result){
+                    console.log("Error: "+ error);
+                }else{
+                    res.redirect("/AdultoMayor");
+                }
+            }
+        )
+    })
+
+
+    app.get("/AdultoMayor", (req, res) => {
+        res.render("../views/post/AdultoMayor.ejs");
+    });
+    app.get("/Baile", (req, res) => {
+        res.render("../views/post/Baile.ejs");
+    });
+    app.get("/BuenComienzo", (req, res) => {
+        res.render("../views/post/BuenComienzo.ejs");
+    });
+    app.get("/Manualidades", (req, res) => {
+        res.render("../views/post/Manualidades.ejs");
+    });
+    app.get("/SinLimites", (req, res) => {
+        res.render("../views/post/SinLimites.ejs");
+    });
+    app.get("/Telecentro", (req, res) => {
+        res.render("../views/post/Telecentro.ejs");
     });
 }
